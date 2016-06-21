41d99859b2fbf31faaefdeaf7d09cf9532e49966
Gareth Bilaney <gareth.bilaney@hhu.de>
2016-06-21 16:44:34 +0200

    Update Save.java

--- Save.java
+++ Save.java
@@ -1,7 +1,13 @@
 public class Save{
 
 public void save(int Status){
-
+  
+  if(Status==0)
+      //speicher text.java;
+  if(Status==1)
+      //kopiere code.java --> temp_code.java
+  if(Status==2)
+       //kopiere temp_code.java --> code.java
 
 }
 
