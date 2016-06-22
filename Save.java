8a0187dc34242104d14fe11c811f8a56589158d9
anjub100 <Andre.Juber@uni-duesseldorf.de>
2016-06-22 19:14:37 +0200

    Der Aufbau von Save.java wurde leicht angepasst. Inhaltlich wurde noch nichts veraendert.

--- Save.java
+++ Save.java
@@ -1,16 +1,29 @@
-public class Save{
+public class Save {
 
-public void save(int Status){
-  
-  if(Status==0)
-      //speicher text.java;
-  if(Status==1)
-      //kopiere code.java --> temp_code.java
-  if(Status==2)
-       //kopiere temp_code.java --> code.java
+	public static void save (int status) {
+		switch (status) {
+			case 0:
+				saveText();
+				break;
+			case 1:
+				codeToTemp();
+				break;
+			case 2:
+				tempToCode();
+				break;
+		}
+	}
 
-}
+	public static void saveText() {
+		// Speichere Text.java;
+	}
 
+	public static void codeToTemp() {
+		// Kopiere Code.java --> Temp_code.java
+	}
 
+	public static void tempToCode() {
+		// Kopiere Temp_code.java --> Code.java
+	}
 
 }
