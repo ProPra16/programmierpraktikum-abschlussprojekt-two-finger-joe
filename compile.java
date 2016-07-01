f512fa1eb4d531bd276e692ca06351b36c03b188
Gareth Bilaney <gareth.bilaney@hhu.de>
2016-07-01 18:40:54 +0200

    Update compile.java

--- compile.java
+++ compile.java
@@ -1,7 +1,20 @@
-public class compile{
-  
-  
-  
-  
-  
+import vk.core.api.CompilationUnit;
+import vk.core.api.CompilerFactory;
+import vk.core.api.JavaStringCompiler;
+import vk.core.internal.InternalResult
+
+public class compile {
+	
+
+	boolean CompileErrors(String className, String classContent){
+		
+	
+	CompilationUnit unit = new CompilationUnit(className, classContent, false);
+	JavaStringCompiler compiler = CompilerFactory.getCompiler(unit);
+	
+	
+	return compiler.hasCompileErrors();
+	
+	}
+
 }
