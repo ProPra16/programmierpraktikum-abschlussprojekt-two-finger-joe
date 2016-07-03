70d75259ef6b120307f749941d6833c19709d069
Gareth Bilaney <gareth.bilaney@hhu.de>
2016-07-03 18:03:42 +0200

    Update

--- Test_Logic.java
+++ Test_Logic.java
@@ -1,3 +1,5 @@
+//import BabySteps.Status;
+
 /* Erste Skizze der Logik Architektur*/
 
 //package de.hhu.propra16.project7;
@@ -15,7 +17,14 @@
 
 
 
-public class Test_Logic {
+public class Logic {
+	
+	private Status Zustand;
+	
+	private boolean Baby;
+	private int Minuten;
+	
+	int seconds;
 	
 	public static void main(String[] args){
 	
@@ -25,57 +34,77 @@ public class Test_Logic {
 
 
 	
-	public Status Input(int Nutzer, 
-				boolean TestFehlschlag, Status status){
+	public void Input(Befehl befehl, 
+				boolean TestFehlschlag, int BabySteps){
 				boolean CompilerWorks = true;
 				
 				
 			//	boolean CompilerWorks = CompileErrors();
 				
-				
+				Status status = getStatus();
 		
-		if(status==Status.Red) return Red( Nutzer,  CompilerWorks,  TestFehlschlag);
-		if(status==Status.Green) return Green( Nutzer,  CompilerWorks,  TestFehlschlag);
-		if(status==Status.Refactoring) return Refactoring( Nutzer,  CompilerWorks,  TestFehlschlag);
+		if(status==Status.Red)  Red( befehl,  CompilerWorks,  TestFehlschlag, status, BabySteps);
+		if(status==Status.Green)  Green( befehl,  CompilerWorks,  TestFehlschlag);
+		if(status==Status.Refactoring)  Refactoring( befehl,  CompilerWorks,  TestFehlschlag);
 		
 		
-		return Status.Red;
+		return;
 	}
 	
 
 	
-	public Status Red(int Nutzer, 
-			boolean CompilerWorks, boolean TestFehlschlag){
+	public void Red(Befehl befehl, 
+			boolean CompilerWorks, boolean TestFehlschlag, Status status, int BabySteps){
 		
-			if(Nutzer==1 && (CompilerWorks==false||TestFehlschlag==true)){	
-				return Status.Green;
-				}
 
-		return Status.Red;
+				if(getBabyBoolean()==true){StartTimer(getStatus());return;}
+		
+			if(befehl==Befehl.DoGreen && (CompilerWorks==false||TestFehlschlag==true)){	
+				setStatus(Status.Green);
+				return;
+				}
+			
+			setStatus(Status.Red);
+
+		return;
 				
 	} 
 	
 	
 
 	
-	public Status Green(int Nutzer, 
+	public void Green(Befehl befehl, 
 			boolean CompilerWorks, boolean TestFehlschlag){
+		
+				if(getBabyBoolean()==true){StartTimer(getStatus());return;}
 
-			if(Nutzer==0)return Status.Red;
-			if(Nutzer==1)return Status.Green;
-			if(Nutzer==2 && CompilerWorks==true && TestFehlschlag == false)return Status.Refactoring;
+
+			if(befehl==Befehl.DoRed){ setStatus(Status.Red); return;}
+			if(befehl==Befehl.DoGreen){ setStatus(Status.Green); return;}
+			if(befehl==Befehl.DoRefactoring && CompilerWorks==true && TestFehlschlag == false){ setStatus(Status.Refactoring); return;}
 			
-			
-			return Status.Green;		
+			setStatus(Status.Green);
+			return;		
 	}
 	
 
 	
 	
-	public Status Refactoring(int Nutzer, 
+	public void Refactoring(Befehl befehl, 
 			boolean CompilerWorks, boolean TestFehlschlag){
-			if(Nutzer==0 && CompilerWorks==true && TestFehlschlag == false)return Status.Red;
-			return Status.Refactoring;		
+			if(befehl==Befehl.DoRed && CompilerWorks==true && TestFehlschlag == false){ setStatus(Status.Red); return;}
+			
+			setStatus(Status.Refactoring);
+			return ;		
+	}
+	
+	public void setStatus(Status status){
+		this.Zustand = status;
+		
+	}
+	
+	public Status getStatus(){
+		return Zustand;
 	}
 	
 	
@@ -86,33 +115,144 @@ public class Test_Logic {
 	
 	} */
 	
-	public enum Status
-	{
-		Red, Green, Refactoring
-	}
+	
+	
+	
+
 	
 	public static void test(){
 		
-		 Status status = Status.Red;
-		 
-			Logic log = new Logic();
-			Status a = log.Input(1,true,status);
-			System.out.println(a);
-			
-			
-			status = Status.Green;
-			 a = log.Input(2,false,status);
-			System.out.println(a);
-			
-			status = Status.Refactoring;
-			 a = log.Input(0,false,status);
-			System.out.println(a);
 		
+			
+		
+			
+			Logic log = new Logic(); 	
+			
+			log.BabySteps(1 ,false);
+			
+			log.setStatus(Status.Red); //Standard
+			
+			log.Input(Befehl.DoGreen,true, 0);
+			
+			System.out.println(log.getStatus());
+			
+			log.Input(Befehl.DoRed,true, 0);
+			
+			System.out.println(log.getStatus());
+			
+			log.Input(Befehl.DoGreen,true, 0);
+			
+			System.out.println(log.getStatus());
+			
+			
+			log.Input(Befehl.DoRefactoring,false, 0);
+			
+			System.out.println(log.getStatus());
+			
+				log.Input(Befehl.DoGreen,false, 0);
+			
+			System.out.println(log.getStatus());
+			
+			log.Input(Befehl.DoRed,false, 0);
+			
+			System.out.println(log.getStatus());
+			
+			log.Input(Befehl.DoRefactoring,false, 0);
+			
+			System.out.println(log.getStatus());
+			
+			log.Input(Befehl.DoGreen,true, 0);
+			
+			System.out.println(log.getStatus());
+			
+	
+	
+		
+	}
+	
+	
+
+
+	
+//BabySteps * * * * * * * * * * * * * * * *     
+	
+	public void BabySteps(int Minuten, boolean Baby ){
+		
+		setMinute(Minuten);
+		setBabyBoolean(Baby);
+		
+	}
+	
+	public void setMinute(int Minuten){
+		this.Minuten = Minuten;
+		
+	}
+	
+	public int getMinute(){
+		return Minuten;
+	}
+	
+	
+	public void setBabyBoolean(boolean Baby){
+		this.Baby = Baby;
+		
+	}
+	
+	public boolean getBabyBoolean(){
+		return Baby;
+	}
+	
+
+	public void StartTimer(Status status ){
+
+		int Minuten = getMinute();
+		long Vergleich = ConvertSeconds(Minuten);
+		 Stoppuhrstarte(status, Vergleich);
+	}
+	
+	public long ConvertSeconds(int Minuten){
+	long Vergleich  = Minuten*6;
+	return Vergleich;
+	}
+	
+	void Stoppuhrstarte(Status status, Long Vergleich)
+	{ 
+		
+		seconds = 0;
+		try {
+		while(seconds<=Vergleich){
+			 Thread.sleep(1000); seconds++;}
+		
+		if(status==Status.Green){;setStatus(Status.Red);}
+		if(status==Status.Red){setStatus(Status.Green);}
+		
+
+		
+		
+		
+		}
+		catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 		
 	}
 	
 	
 	
+//ENUM * * * * * * * * * * * * * * * *            
+
+public enum Status
+{
+	Red, Green, Refactoring
+}
+
+public enum Befehl
+{
+	DoRed, DoGreen, DoRefactoring
+}
+
+
+
 	
 		
 }
