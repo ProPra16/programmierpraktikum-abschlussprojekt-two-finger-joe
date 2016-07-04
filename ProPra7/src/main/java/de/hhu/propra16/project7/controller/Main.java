bbf6e9e77654985cb9aaba57ca397009e78692d3
barbieAuglend <barbieauglend@icloud.com>
2016-07-04 16:50:54 +0200

    Main Class fertig

---
+++ ProPra7/src/main/java/de/hhu/propra16/project7/controller/Main.java
@@ -0,0 +1,30 @@
+package de.hhu.propra16.project7.controller;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.*;
+	 
+public class Main extends Application
+{
+	public static void main(String[] args)
+	{
+		new Logic();
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws IOException
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
+
+		Scene scene = new Scene(root, 640, 480);
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.setTitle("TDDT");
+		stage.show();
+	}
+	
+}
