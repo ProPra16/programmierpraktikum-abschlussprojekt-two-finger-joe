95609683ac8b872d504af3bdba77661981ccbc79
Gareth Bilaney <gareth.bilaney@hhu.de>
2016-07-07 16:09:37 +0200

    Back Up - Delete

---
+++ Delete.java
@@ -0,0 +1,12 @@
+
+public class Delete {
+	
+//	if(status==Logic.Status.BabyRed)        
+	//return DeleteTest(project);
+	
+	
+//	if(status==Logic.Status.BabyGreen)		 
+	//return DeleteCode(project);
+	
+
+}
