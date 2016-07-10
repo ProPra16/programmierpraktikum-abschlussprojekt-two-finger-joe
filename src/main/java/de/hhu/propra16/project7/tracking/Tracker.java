d392c930e939060a7c74b5f3e43f115111c5213f
anjub100 <Andre.Juber@uni-duesseldorf.de>
2016-07-10 18:38:17 +0200

    The Tracker can now receive error messages and write them into the log file.

--- src/main/java/de/hhu/propra16/project7/tracking/Tracker.java
+++ src/main/java/de/hhu/propra16/project7/tracking/Tracker.java
@@ -8,6 +8,7 @@ import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import de.hhu.propra16.project7.controller.Status;
@@ -28,24 +29,60 @@ public class Tracker {
 		String change = getTime() + " - Status changed to ";
 		switch(status) {
 			case Red:
-				change += "Red";
+				change += "Red (Babysteps OFF).";
+				break;
+			case BabyRed:
+				change += "Red (Babysteps ON).";
 				break;
 			case Green:
-				change += "Green";
+				change += "Green (Babysteps OFF).";
+				break;
+			case BabyGreen:
+				change += "Green (Babysteps ON).";
 				break;
 			case Refactoring:
-				change += "Refactoring";
+				change += "Refactoring.";
 				break;
 		}
 		final List<String> content = new ArrayList<>();
 		content.add(change);
 		saveChanges(content);
+		newLine();
+	}
+
+	// Adds a compilation error message to the log file
+	public void compilationFailed(String error) throws IOException {
+		final List<String> message = new ArrayList<>();
+		message.add(getTime() + " - Compilation failed:");
+		message.add("");
+		saveChanges(message);
+		String[] lines = error.split("\n");
+		for (int i = 0; i < lines.length; i++) {
+			for (int j = 0; j < 23; j++) {
+				lines[i] = " " + lines[i];
+			}
+		}
+		final List<String> content = Arrays.asList(lines);
+		saveChanges(content);
+		newLine();
+	}
+
+	// Returns the project name
+	public String getProjectName() {
+		return project;
+	}
+
+	// Adds a new line to the log file
+	private void newLine() throws IOException {
+		final List<String> newline = new ArrayList<>();
+		newline.add("");
+		saveChanges(newline);
 	}
 
 	// Adds text messages to the log file
 	private void saveChanges(List<String> content) throws IOException {
-		createPath("/de/hhu/propra16/tracking/projects/" + project);
-		String path = System.getProperty("user.dir") + "/de/hhu/propra16/tracking/projects/" + project + "/log.txt";
+		createPath("/data/" + project);
+		String path = System.getProperty("user.dir") + "/data/" + project + "/log.txt";
 		final Path p = Paths.get(path);
 		if (!Files.exists(p)) {
 			Files.write(p, content);
@@ -58,7 +95,7 @@ public class Tracker {
 	// Gets the time for the log file
 	private String getTime() {
 		LocalDateTime time = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd. MM. yyyy, HH:mm:ss");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss");
 		return time.format(formatter);
 	}
 
